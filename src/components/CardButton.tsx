795276053b2db9b39cc3b0d25ecdbf74394007a2
Nivethan <arnive19@gmail.com>
2021-12-25 22:19:27 +0530

    Fix cardButton ui

--- src/components/CardButton.tsx
+++ src/components/CardButton.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 const CardButton = (props:any) => {
   const { btnLabel } = props;
   return (
-    <div className="">
-      <h6 className="text-xs bg-fuchsia-800 w-16 rounded-lg px-1">
+    // eslint-disable-next-line max-len
+    <div className="w-12 text-[0.5rem] text-center bg-gradient-to-r from-[#6e267c] to-[#be65bc] rounded-md text-white font-semibold py-[2px]">
+      <h6>
         {btnLabel}
       </h6>
     </div>
