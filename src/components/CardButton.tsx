eb0608441b6e789c831e4ec5ba6665738328af34
Nivethan <arnive19@gmail.com>
2021-12-26 19:59:57 +0530

    Fix CardButton

--- src/components/CardButton.tsx
+++ src/components/CardButton.tsx
@@ -4,8 +4,8 @@ const CardButton = (props:any) => {
   const { btnLabel } = props;
   return (
     // eslint-disable-next-line max-len
-    <div className="w-12 text-[0.5rem] text-center bg-gradient-to-r from-[#6e267c] to-[#be65bc] rounded-md text-white font-semibold py-[2px]">
-      <h6>
+    <div className="w-max px-2 text-[0.5rem] cursor-pointer text-center bg-gradient-to-r from-[#6e267c] to-[#be65bc] rounded-md text-white font-medium py-[2px]">
+      <h6 className="uppercase tracking-wider">
         {btnLabel}
       </h6>
     </div>
