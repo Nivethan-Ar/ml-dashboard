f87beebca03236a992d340bb33a1d8e2210c394f
Nivethan <arnive19@gmail.com>
2021-12-26 19:59:31 +0530

    Fix NavCardItem

--- src/layouts/Sidebar/Navigation/NavCardItem.tsx
+++ src/layouts/Sidebar/Navigation/NavCardItem.tsx
@@ -9,13 +9,13 @@ const NavCardItem = (props:any) => {
     <li>
       <Link to={link}>
         <div
-          className="bg-[#152042] rounded-xl px-6 py-2 mx-3  text-white"
+          className="bg-[#152042] rounded-xl px-6 py-2 mx-3 shadow-lg shadow-violet-600/30 hover:shadow-violet-600/70 text-white"
         >
           <div className="mb-[-8px]">
             <CardButton btnLabel="ALL NEW" />
           </div>
           <div className="flex mt-0 font-bold align-top">
-            <div className="flex flex-col justify-center text-sm leading-5 tracking-wide">
+            <div className="flex flex-col justify-center text-sm leading-snug tracking-wide">
               <h6>Artificial</h6>
               <h6>Intelligence</h6>
               <h6>Dashboard</h6>
