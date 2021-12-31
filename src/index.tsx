1fa5f7c4b887eeff2c253e200374a3d6d246c845
Nivethan <arnive19@gmail.com>
2021-12-31 17:07:23 +0530

    Redux toolkit initial setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import ReactDOM from 'react-dom';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import { store } from './app/store';
+
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
