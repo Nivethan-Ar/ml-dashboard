beeffa05c1605ab9d3aefa2028db2e41b5c16e55
Nivethan <arnive19@gmail.com>
2022-01-05 09:40:11 +0530

    Fine-tune NavCardItem

--- src/components/CardButton.tsx
+++ src/components/CardButton.tsx
@@ -4,10 +4,10 @@ const CardButton = (props:any) => {
   const { btnLabel } = props;
   return (
     // eslint-disable-next-line max-len
-    <div className="w-max px-2 text-[0.5rem] cursor-pointer text-center bg-gradient-to-r from-[#6e267c] to-[#be65bc] rounded-md text-white font-medium py-[2px]">
-      <h6 className="uppercase tracking-wider">
+    <div className="w-max px-2 text-[0.45rem] cursor-pointer text-center bg-gradient-to-r from-[#6e267c] to-[#be65bc] rounded-md text-white font-medium py-[2px]">
+      <p className="uppercase tracking-wider">
         {btnLabel}
-      </h6>
+      </p>
     </div>
   );
 };
--- src/layouts/Sidebar/Navigation/NavCardItem.tsx
+++ src/layouts/Sidebar/Navigation/NavCardItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CardIcon from './ai.png';
+// import CardIcon2 from './head.png';
 import CardButton from '../../../components/CardButton';
 
 const NavCardItem = (props:any) => {
@@ -9,18 +10,18 @@ const NavCardItem = (props:any) => {
     <li>
       <Link to={link}>
         <div
-          className="bg-[#152042] rounded-xl px-6 py-2 mx-3 shadow-lg shadow-violet-600/30 hover:shadow-violet-600/70 text-white"
+          className="bg-[#152042] rounded-xl pl-5 pt-3 pb-1 mx-2 shadow-lg shadow-violet-600/30 hover:shadow-violet-600/70 text-white"
         >
-          <div className="mb-[-8px]">
+          <div className="mb-[-10px]">
             <CardButton btnLabel="ALL NEW" />
           </div>
-          <div className="flex mt-0 font-bold align-top">
-            <div className="flex flex-col justify-center text-sm leading-snug tracking-wide">
+          <div className="flex mt-0 font-bold align-top gap-2">
+            <div className="flex flex-col justify-center text-xs leading-snug tracking-wide">
               <h6>Artificial</h6>
               <h6>Intelligence</h6>
               <h6>Dashboard</h6>
             </div>
-            <img className="w-20" src={CardIcon} alt="" />
+            <img className="w-[4.5rem] " src={CardIcon} alt="" />
           </div>
         </div>
       </Link>
