dd3a2458c3eef3d6532561a4e01df7119b714441
Nivethan <arnive19@gmail.com>
2022-01-09 18:59:23 +0530

    Add new sidebar effects to branding section

--- src/layouts/Sidebar/Branding/index.tsx
+++ src/layouts/Sidebar/Branding/index.tsx
@@ -1,10 +1,12 @@
 import logo from './salessuite-logo-dark.png';
 import MenuIcon from './menu.png';
+import TwcIcon from './twc-logo.png';
 
 const Branding = () => (
-  <div className="mt-5 mb-6 p-2 flex items-center justify-between w-full">
-    <img src={logo} alt="salessuite logo" className="w-4/6" />
-    <img src={MenuIcon} className="w-4 h-5" alt="menu icon" />
+  <div className="mt-4 mb-0 p-2 flex items-center justify-between w-full">
+    <img src={TwcIcon} alt="" className="group-hover:hidden w-14" />
+    <img src={logo} alt="salessuite logo" className="w-4/6 hidden group-hover:flex" />
+    <img src={MenuIcon} className="w-4 h-5 hidden group-hover:block" alt="menu icon" />
   </div>
 );
 
