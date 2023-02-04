5c4c00bd898978fc5962df98e84b9f34a63f8369
Nivethan <63112138+Nivethan-Ar@users.noreply.github.com>
2023-02-04 23:50:37 +0530

    fix: disable private routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import PrivateRoute from './routes/PrivateRoute';
-import PublicRoute from './routes/PublicRoute';
-import SetLocalStorage from './pages/login/SetLocalStorage';
 import Dashboard from './pages/DashboardML/Dashboard';
+import Development from './pages/DashboardML/Development';
+import Incentives from './pages/DashboardML/Incentives';
+import Productivity from './pages/DashboardML/Productivity';
+import SetLocalStorage from './pages/login/SetLocalStorage';
 import Manage from './pages/Manage/Manage';
-import Login from './pages/login/Login';
 import Sub1 from './pages/Manage/sub1/Sub1';
 import Sub2 from './pages/Manage/sub2/Sub1';
-import Productivity from './pages/DashboardML/Productivity';
-import Incentives from './pages/DashboardML/Incentives';
-import Development from './pages/DashboardML/Development';
+import PrivateRoute from './routes/PrivateRoute';
 
 const App = () => (
   <div>
     <Routes>
-      <Route path="/" element={<PublicRoute />}>
+      {/* <Route path="/" element={<PublicRoute />}>
         <Route path="login" element={<Login />} />
-      </Route>
+      </Route> */}
 
       <Route path="/" element={<PrivateRoute />}>
-        <Route path="dashboard" element={<Dashboard />}>
+        <Route path="/" element={<Dashboard />}>
           <Route index element={<Productivity />} />
           <Route path="incentives" element={<Incentives />} />
           <Route path="development" element={<Development />} />
--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,12 +1,13 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
-import { Rootstate } from '../app/store';
+// import { Rootstate } from '../app/store';
 
 const PrivateRoute = () => {
   const location = useLocation();
 
-  const user = useSelector((state:Rootstate) => state.userAuth.user);
+  // const user = useSelector((state:Rootstate) => state.userAuth.user);
+  const user = true;
 
   return user
     ? <Outlet />
